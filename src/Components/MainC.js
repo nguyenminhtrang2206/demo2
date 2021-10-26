dbb94ed9da1525742d33d03f9bfd14ade8f9a130
BC-macair020 <bc-macair020@BC-macair020s-MacBook-Air.local>
2021-10-26 13:16:51 +0300

    using array map

--- src/Components/MainC.js
+++ src/Components/MainC.js
@@ -1,30 +1,14 @@
-// import React, { Component } from "react";
-// import Box from "./Box"
-
-// class MainC extends Component {
-//     render() {
-//         return (
-//             <main>
-//                 <Box />
-//                 <Box />
-//                 <Box />
-
-//             </main>
-//         );
-//     }
-// }
-
-// export default MainC;
-
 import React, { Component } from "react";
 import Box from "./Box";
 
 class MainC extends Component {
   state = {
     persons: [
-      { name: "Margit", age: 25, title: "CEO" },
-      { name: "Kati", age: 35, title: "Designer" },
-      { name: "Mari", age: 40, title: "Developer" },
+      { id: 1, name: "Margit", age: 25, title: "CEO" },
+      { id: 2, name: "Kati", age: 35, title: "Designer" },
+      { id: 3, name: "Mari", age: 40, title: "Developer" },
+      { id: 4, name: "Mari", age: 40, title: "Developer" },
+
     ],
   };
 
@@ -35,25 +19,11 @@ class MainC extends Component {
   render() {
     return (
       <>
-      <main>
-        <Box
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
-          title={this.state.persons[0].title}
-        />
-        <Box
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-          title={this.state.persons[1].title}
-        />
-        <Box
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age}
-          title={this.state.persons[2].title}
-        />
-      </main>
-      <button onClick = {this.changeNameHandler}>Click me</button>
-      
+        <main>
+          {this.state.persons.map((item) => ( 
+          <Box key ={item.id} name={item.name} age={item.age} title={item.title} />
+          ))}
+        </main>
       </>
     );
   }
